6bbb1c1f594c9888a3785a421bc7354ce6cb4dd2
tienpm <minhtienit1997@gmail.com>
2021-05-19 22:25:24 +0700

    init

---
+++ src/main/java/com/example/githubactionexample/GithubActionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionExampleApplication.class, args);
+	}
+
+}
